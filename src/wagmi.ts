15b4a8ef1629d1fec80286716c7ae8f23a8696f9
Christian Laino <christian@fleek.co>
2022-12-08 15:37:23 +0100

    basic config

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,9 +1,23 @@
 import { getDefaultClient } from 'connectkit'
-import { createClient } from 'wagmi'
+import { configureChains, createClient } from 'wagmi'
+import { polygonMumbai, arbitrumGoerli } from 'wagmi/chains'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+
+import { ENV } from './config'
+
+const alchemyApiKey = ENV.alchemyApiKey
+const allowedChains = [polygonMumbai, arbitrumGoerli]
+
+const { chains, provider } = configureChains(allowedChains, [
+  alchemyProvider({ apiKey: alchemyApiKey }),
+])
 
 export const client = createClient(
   getDefaultClient({
+    appName: 'blobs',
     autoConnect: true,
-    appName: 'My wagmi + ConnectKit App',
+    connectors: [new MetaMaskConnector({ chains })],
+    provider,
   })
 )
