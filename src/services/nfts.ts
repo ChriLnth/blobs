1b446cb3d5fe2f0414bf859c6b7ffdbd0d1149fe
Christian Laino <christian@fleek.co>
2022-12-09 22:17:57 +0100

    get nfts

---
+++ src/services/nfts.ts
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import { ENV } from '@/config'
+
+const apiKey = ENV.alchemyApiKey
+const baseURL = `https://polygon-mumbai.g.alchemy.com/nft/v2/${apiKey}`
+
+export const getUserBlobs = async (address: string) => {
+  const response = await axios({
+    method: 'get',
+    url: `${baseURL}/getNFTs/?owner=${address}`
+  })
+
+  if (response && response.data) {
+    const blobs = response.data.ownedNfts.filter(
+      (nft: { contract: { address: string } }) =>
+        nft.contract.address === ENV.contracts.blobs.toLowerCase()
+    )
+
+    const formatted = blobs.map(
+      (b: {
+        id: { tokenId: string };
+        metadata: { name: string; image: string }
+      }) => {
+        return {
+          id: parseInt(b.id.tokenId, 16),
+          name: b.metadata.name,
+          image: b.metadata.image,
+        }
+      })
+      
+      return formatted
+  }
+
+  return []
+}
+
+export const getBlobMetadata = async (tokenId: string) => {
+  const contract = ENV.contracts.blobs
+  const url = `${baseURL}/getNFTMetadata?contractAddress=${contract}&tokenId=${tokenId}&tokenType=erc721`
+
+  const response = await axios({
+    method: 'get',
+    url,
+    headers: {}
+  })
+
+  console.log('response', response.data)
+}
